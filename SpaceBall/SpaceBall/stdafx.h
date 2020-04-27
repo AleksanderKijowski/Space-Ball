65e1ad6dff9c40fa652f965a68abfdb83500b758
Aleksander Kijowski <akijowski@ess.etisoft.pl>
2020-04-27 14:10:22 +0200

    Added precompiled header file.

---
+++ SpaceBall/SpaceBall/stdafx.h
@@ -0,0 +1,22 @@
+#pragma once
+
+// SFML libraries
+#include <SFML\Graphics.hpp>
+#include <SFML\Audio.hpp>
+#include <SFML\System.hpp>
+#include <SFML\Window.hpp>
+
+// Standard C++ 
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <thread>
+#include <math.h>
+#include <Windows.h>
+
+// Containers
+#include <list>
+#include <map>
+#include <stack>
+#include <vector>

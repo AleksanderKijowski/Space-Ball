53ff30ffe67fc9f80c6cd367afe35b1758a76406
AleksanderKijowski <43816932+AleksanderKijowski@users.noreply.github.com>
2020-04-28 14:16:15 +0200

    Revert "Feature/#005"

--- SpaceBall/SpaceBall/ConfigPaths.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-const string WindowSettingsPath = "../Configs/Window Settings.ini";
-
-const string SupportedKeysPath = "../Configs/Supported keys.ini";
-
-const string ExceptionSavingDirectoryPath = "../Logs";

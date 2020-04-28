b40d400f9e9ad082ac8124235037e99465bebaff
Aleksander Kijowski <akijowski@ess.etisoft.pl>
2020-04-28 22:25:47 +0200

    Added game context and IState classes.

--- SpaceBall/SpaceBall/Game.hpp
+++ SpaceBall/SpaceBall/Game.hpp
@@ -1,16 +1,13 @@
 #pragma once
 #include "stdafx.h"
-#include "RenderWindowBuilder.hpp"
 #include "ExceptionHandlerBuilder.hpp"
-#include "SupportedKeysMapBuilder.hpp"
+#include "GameContext.hpp"
 
 class Game
 {
 private:
 	std::unique_ptr<IExceptionHandler> _exceptionHandler;
-	std::unique_ptr<sf::RenderWindow> _window;
-	std::unique_ptr<Map<string, int>> _supportedKeys;
-
+	std::unique_ptr<GameContext> _context;
 	sf::Event _event;
 
 public:
@@ -23,64 +20,39 @@ public:
 
 	void Run()
 	{
-		if (_exceptionHandler != nullptr && Initialize())
+		if (_exceptionHandler != nullptr)
 		{
-			RunGameLoop();
+			try
+			{
+				_context.reset(new GameContext());
+
+				RunGameLoop();
+			}
+			catch(Exception* exception)
+			{
+				_exceptionHandler->HandleException(exception);
+			}
 		}
 	}
 
 private:
-	void UpdateEvents()
-	{
-		while (_window->pollEvent(_event))
-		{
-			if (_event.type == sf::Event::Closed)
-			{
-				_window->close();
-			}
-		}
-	}
-	
 	void Update()
 	{
-		
+		_context->GetCurrentAppState()->Update();
 	}
 	
 	void Render()
 	{
-		_window->clear();
-		_window->display();
+		_context->GetCurrentAppState()->Render();
 	}
 
 	void RunGameLoop()
 	{
-		try
-		{	
-			while (_window != nullptr && _window->isOpen())
-			{
-				UpdateEvents();
-				Update();
-				Render();
-			}
-		}
-		catch (Exception* exception)
+		while (!_context->IsAppEnding())
 		{
-			_exceptionHandler->HandleException(exception);
-		}
-	}
-
-	bool Initialize()
-	{
-		try
-		{
-			_window.reset(RenderWindowBuilder().BuildRenderWindow());
-			_supportedKeys.reset(SupportedKeysMapBuilder().BuildSupportedKeysMap());
-			return true;
-		}
-		catch (Exception* exception)
-		{
-			_exceptionHandler->HandleException(exception);
-			return false;
+			_context->UpdateEvents();
+			Update();
+			Render();
 		}
 	}
 };
